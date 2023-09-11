58d98ae07a16d665b91e2f2acd29d3238335d456
DivadNojnarg <dgranjon@ymail.com>
2023-09-11 15:26:30 +0200

    update

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # complexInput
 
-To run the demo, `devtools::load_all()` and run `./app.R`. This requires `{shiny}` `1.7.5` since we rely on `data-shiny-no-bind-input` [feature](https://github.com/rstudio/shiny/blob/main/NEWS.md#new-features-and-improvements-1) to prevent Shiny from binding usual input
+To run the demo, `devtools::load_all()` and run `./app.R`. Then, paste one of those urls and look at the widget state:
+
+```bash
+# First choice: http://127.0.0.1:4082/?_inputs_&plop_config=%7B%22pickerOpts%22%3A%7B%22multiple%22%3Atrue%2C%22dataMaxOptions%22%3A2%2C%22val%22%3A%22Hot%20Dog%2C%20Fries%20and%20a%20Soda%22%7D%2C%22textOpts%22%3A%7B%22label%22%3A%22My%20super%20text%20Input%22%2C%22placeholder%22%3A%22my%20custom%20placeholder%22%7D%7D
+
+# Third choice: http://127.0.0.1:4082/?_inputs_&plop_config=%7B%22pickerOpts%22%3A%7B%22multiple%22%3Atrue%2C%22dataMaxOptions%22%3A2%2C%22val%22%3A%5B%22Sugar%2C%20Spice%20and%20all%20things%20nice%22%5D%7D%2C%22textOpts%22%3A%7B%22label%22%3A%22My%20super%20text%20Input%22%2C%22placeholder%22%3A%22my%20custom%20placeholder%22%7D%7D
+```
+
+
+## Notes
+
+This requires `{shiny}` `1.7.5` since we rely on `data-shiny-no-bind-input` [feature](https://github.com/rstudio/shiny/blob/main/NEWS.md#new-features-and-improvements-1) to prevent Shiny from binding usual input
 elements.
--- app.R
+++ app.R
@@ -13,6 +13,7 @@ server <- function(input, output, session) {
   })
   
   onBookmarked(function(url) {
+    # TO DO: encode URL to reduce nchar ...
     updateQueryString(url)
   }) 
   
@@ -39,8 +40,9 @@ server <- function(input, output, session) {
     } else {
       # Load from bookmarked config
       # Try the below URL to restore a given widget state
-      # http://127.0.0.1:4082/?_inputs_&plop_config=%7B%22pickerOpts%22%3A%7B%22multiple%22%3Atrue%2C%22dataMaxOptions%22%3A2%2C%22val%22%3A%22Hot%20Dog%2C%20Fries%20and%20a%20Soda%22%7D%2C%22textOpts%22%3A%7B%22label%22%3A%22My%20super%20text%20Input%22%2C%22placeholder%22%3A%22my%20custom%20placeholder%22%7D%7D
-      vals$widget_config
+      # First choice: http://127.0.0.1:4082/?_inputs_&plop_config=%7B%22pickerOpts%22%3A%7B%22multiple%22%3Atrue%2C%22dataMaxOptions%22%3A2%2C%22val%22%3A%22Hot%20Dog%2C%20Fries%20and%20a%20Soda%22%7D%2C%22textOpts%22%3A%7B%22label%22%3A%22My%20super%20text%20Input%22%2C%22placeholder%22%3A%22my%20custom%20placeholder%22%7D%7D
+      # Third choice: http://127.0.0.1:4082/?_inputs_&plop_config=%7B%22pickerOpts%22%3A%7B%22multiple%22%3Atrue%2C%22dataMaxOptions%22%3A2%2C%22val%22%3A%5B%22Sugar%2C%20Spice%20and%20all%20things%20nice%22%5D%7D%2C%22textOpts%22%3A%7B%22label%22%3A%22My%20super%20text%20Input%22%2C%22placeholder%22%3A%22my%20custom%20placeholder%22%7D%7D
+       vals$widget_config
     }
     
     complexInput(
@@ -52,4 +54,4 @@ server <- function(input, output, session) {
   
 }
 
-shinyApp(ui, server, enableBookmarking = "url")
+shinyApp(ui, server, enableBookmarking = "url", options = list("shiny.port" = 4082))
